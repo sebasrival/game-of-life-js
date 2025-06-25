0c8b74fb52b0e7e0b8bd4d9ceddbe47d420ab416
Sebastian Rivaldi <sebasrival@gmail.com>
2025-06-25 00:32:28 -0300

    feat: actualizar dimensiones y colores en la configuración y la posicion de los vecinos

--- src/config.js
+++ src/config.js
@@ -1,14 +1,27 @@
-const WSIZE = 600;
-const HSIZE = 600;
+const WSIZE = 1000;
+const HSIZE = 1000;
 
 const CELL_SIZE = 10;
 const ROWS = HSIZE / CELL_SIZE;
 const COLS = WSIZE / CELL_SIZE;
 
 const BG_COLOR = "#242424";
+const BG_COLOR_LIGHT = "white";
 const COLOR_LIVE = "#ffffff";
+const COLOR_LIVE_LIGHT = "black";
 const BORDER_COLOR = "grey";
 const DEAD_COLOR = BG_COLOR;
+
+const NEIGHBORS_POS = [
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+  [-1, 0],
+  [1, 0],
+  [-1, 1],
+  [0, 1],
+  [1, 1],
+];
 
 export {
   WSIZE,
@@ -20,4 +33,7 @@ export {
   COLOR_LIVE,
   DEAD_COLOR,
   BORDER_COLOR,
+  BG_COLOR_LIGHT,
+  COLOR_LIVE_LIGHT,
+  NEIGHBORS_POS,
 };
